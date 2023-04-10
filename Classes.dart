58a4a3971ed42fdc23ff112aa742628ccdaaf503
Yiming Huang <hym20030128@petalmail.com>
2023-04-10 02:20:22 +0000

    new file:   Classes.dart

---
+++ Classes.dart
@@ -0,0 +1,51 @@
+void main(){
+var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
+voyager.describe();
+
+var voyager3 = Spacecraft.unlaunched('Voyager III');
+voyager3.describe();
+
+}
+
+
+class Spacecraft {
+  String name;
+  DateTime? launchDate;//?就是声明了一个可空的变量
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);//构造函数
+
+  // Method.   函数和方法：函数与对象无关，方法和对象相关
+  void describe() {
+    print('Spacecraft: $name');
+    // Type promotion doesn't work on getters.
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
+
+//大道至简，天道酬勤
+
+
+
+
+
+
+
+
+
+}
